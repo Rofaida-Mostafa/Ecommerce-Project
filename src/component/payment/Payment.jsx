257c63a1b4de8cbb758fa9f7d6cd5bbda77339fc
Rofaida-Ashraf <156888802+Rofaida-Mostafa@users.noreply.github.com>
2024-09-02 19:47:16 +0300

    Update Payment.jsx

--- src/component/payment/Payment.jsx
+++ src/component/payment/Payment.jsx
@@ -207,7 +207,7 @@ export default function Payment() {
                 setIsonline(false);
               }}
               type="submit"
-              className="mr-5 text-slate-100 hover:text-blue-100 bg-slate-600 hover:bg-slate-500 focus:ring-4 focus:outline-none focus:ring-blue-300  rounded-lg text-sm font-semibold w-full sm:w-auto px-5 py-2.5 text-center dark:bg-slate-600 dark:hover:bg-slate-500 dark:focus:ring-slate-300"
+              className="md:mb-0 mb-2 mr-4 text-slate-100 hover:text-blue-100 bg-slate-600 hover:bg-slate-500 focus:ring-4 focus:outline-none focus:ring-blue-300  rounded-lg text-sm font-semibold w-full sm:w-auto px-5 py-2.5 text-center dark:bg-slate-600 dark:hover:bg-slate-500 dark:focus:ring-slate-300"
             >
               Pay cash
             </button>
@@ -217,7 +217,7 @@ export default function Payment() {
                 setIsonline(true);
               }}
               type="submit"
-              className="mx-5 text-slate-100 hover:text-blue-100 bg-slate-600 hover:bg-slate-500 focus:ring-4 focus:outline-none focus:ring-blue-300  rounded-lg text-sm font-semibold w-full sm:w-auto px-5 py-2.5 text-center dark:bg-slate-600 dark:hover:bg-slate-500 dark:focus:ring-slate-300"
+              className="md:mx-5 text-slate-100 hover:text-blue-100 bg-slate-600 hover:bg-slate-500 focus:ring-4 focus:outline-none focus:ring-blue-300  rounded-lg text-sm font-semibold w-full sm:w-auto px-5 py-2.5 text-center dark:bg-slate-600 dark:hover:bg-slate-500 dark:focus:ring-slate-300"
             >
               Pay online
             </button>
